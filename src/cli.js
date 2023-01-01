7238352863a1fa5539bda22d632501d5e68ef5ff
Aleksandr Manujlo <dregmar@bk.ru>
2023-01-01 14:53:10 +0530

    add export to cli.js and call to brain-games.js

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,7 @@
 import readlineSync from 'readline-sync';
-const userName = readlineSync.question('May I have your name? ')
-console.log(`Hello, ${userName}!`)
+
+const whatsName = () => {
+    const userName = readlineSync.question('May I have your name? ')
+    console.log(`Hello, ${userName}!`)
+}
+export default whatsName
